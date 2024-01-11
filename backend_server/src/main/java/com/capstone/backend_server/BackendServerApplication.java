ad79f8ec682d6c2fe310e10c9b54e7dd4ec2b0b8
Hezekiah Adeusi <hezekiaha58@gmail.com>
2024-01-11 12:55:08 +0000

    Created backend server Spring application skeleton

---
+++ backend_server/src/main/java/com/capstone/backend_server/BackendServerApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.backend_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendServerApplication.class, args);
+	}
+
+}
