8859fc28d2668cb38e7f17a3b6d277e15bcfa9e5
Rosie Miall <rosiemiall38@gmail.com>
2024-01-12 15:05:30 +0000

    requests for get all delivery addresses and get all route and get all routes by id in controllers and service

--- backend_server/src/main/java/com/capstone/backend_server/controllers/RouteController.java
+++ backend_server/src/main/java/com/capstone/backend_server/controllers/RouteController.java
@@ -1,13 +1,15 @@
 package com.capstone.backend_server.controllers;
 
 import com.capstone.backend_server.models.Route;
+import com.capstone.backend_server.models.Warehouse;
 import com.capstone.backend_server.services.RouteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/routes")
@@ -20,5 +22,19 @@ public class RouteController {
     public ResponseEntity<Route> createRoutes(){
         routeService.optimiseRoutes();
         return new ResponseEntity<>(null, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Route>> getAllRoutes(){
+        return new ResponseEntity<>(routeService.getAllRoutes(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Route> getRouteById(@PathVariable Long id) {
+        Optional<Route> routeByID = routeService.getRouteById(id);
+        return new ResponseEntity<>(
+                routeByID.orElse(null),
+                routeByID.isEmpty() ? HttpStatus.NOT_FOUND : HttpStatus.OK
+        );
     }
 }
--- backend_server/src/main/java/com/capstone/backend_server/services/RouteService.java
+++ backend_server/src/main/java/com/capstone/backend_server/services/RouteService.java
@@ -1,9 +1,18 @@
 package com.capstone.backend_server.services;
 
+import com.capstone.backend_server.models.Route;
+import com.capstone.backend_server.repositories.RouteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RouteService {
+
+    @Autowired
+    RouteRepository routeRepository;
 
     public RouteService() {
     }
@@ -11,5 +20,13 @@ public class RouteService {
     public void optimiseRoutes() {
 
 
+    }
+
+    public List<Route> getAllRoutes() {
+        return routeRepository.findAll();
+    }
+
+    public Optional<Route> getRouteById(Long id) {
+        return routeRepository.findById(id);
     }
 }
