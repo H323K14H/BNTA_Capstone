904cfbb16b89b4354dda051e79b7c1611a554c63
Emmanuel Ajayi <emmanuelajayi380@yahoo.co.uk>
2024-01-12 14:12:35 +0000

    Refactored code to remove uncessary code block

--- frontend_client/src/components/MapComponent.js
+++ frontend_client/src/components/MapComponent.js
@@ -1,51 +1,47 @@
 import React, { useEffect, useState, useRef } from "react";
 import L from "leaflet";
 import {
-  TileLayer,
-  MapContainer,
-  LayersControl
+    TileLayer,
+    MapContainer,
+    LayersControl
 } from "react-leaflet";
 
 import RoutingMachine from "./RoutingMachine";
 
-const maps = {
-  base: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-};
-
 const MapComponent = () => {
-  const [map, setMap] = useState(null);
-  const [start, setStart] = useState([38.9072, -77.0369])
-  const [end, setEnd] = useState([37.7749, -122.4194])
+    const [map, setMap] = useState(null);
+    const [start, setStart] = useState([38.9072, -77.0369]);
+    const [end, setEnd] = useState([37.7749, -122.4194]);
 
-  return (
-    <>
-      <MapContainer
-        center={[37.0902, -95.7129]}
-        zoom={3}
-        zoomControl={false}
-        style={{ height: "100vh", width: "100%", padding: 0 }}
-        whenCreated={map => setMap(map)}
-      >
-        {/* *************** */}
-        {/* Pass in our custom control layer here, inside of the map container */}
-        {/* *************** */}
-        <RoutingMachine 
-          position={'topleft'} 
-          start={start} 
-          end={end} 
-          color={'#757de8'} 
-        />
-        <LayersControl position="topright">
-          <LayersControl.BaseLayer checked name="Map">
-            <TileLayer
-              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-              url={maps.base}
-            />
-          </LayersControl.BaseLayer>
-        </LayersControl>
-      </MapContainer>
-    </>
-  );
+    return (
+        <>
+            <MapContainer
+                center={[37.0902, -95.7129]}
+                zoom={3}
+                zoomControl={false}
+            >
+                {/* *************** */}
+                {/* Pass in our custom control layer here, inside of the map container */}
+                {/* *************** */}
+                <RoutingMachine
+                    position={'topleft'}
+                    start={start}
+                    end={end}
+                    color={'#757de8'}
+                />
+                <LayersControl position="topright">
+                    <LayersControl.BaseLayer checked name="Map">
+                        <TileLayer
+                            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url="http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}"
+                            maxZoom={20}
+                            subdomains={["mt0", "mt1", "mt2", "mt3"]}
+                        />
+                    </LayersControl.BaseLayer>
+                </LayersControl>
+            </MapContainer>
+        </>
+    );
 };
 
 export default MapComponent;
