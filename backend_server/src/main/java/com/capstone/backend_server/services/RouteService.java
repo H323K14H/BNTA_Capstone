5be005fe82e42d949bda557e60556ec53f673440
Hezekiah Adeusi <hezekiaha58@gmail.com>
2024-01-12 16:55:03 +0000

    Slapped in some inspo for the backend API implementation

--- backend_server/src/main/java/com/capstone/backend_server/services/RouteService.java
+++ backend_server/src/main/java/com/capstone/backend_server/services/RouteService.java
@@ -5,6 +5,7 @@ import com.capstone.backend_server.repositories.RouteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,6 +20,35 @@ public class RouteService {
 
     public void optimiseRoutes() {
 
+//        HashMap<String, Object> carAttributes = new HashMap<>();
+//        carAttributes.put("year", 2019);
+//        carAttributes.put("price", 49999.99);
+//        carAttributes.put("colour", "blue");
+//        Car car1 = new Car("BWM x5", carAttributes);
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        String requestBody = objectMapper.writeValueAsString(car1);
+//
+//        URL url = new URL("https://api.restful-api.dev/objects");
+//        HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+//        conn.setRequestMethod("POST");
+//        conn.setDoOutput(true);
+//        conn.setRequestProperty("Content-Type", "application/json");
+//        conn.setRequestProperty("User-Agent", "Mozilla/5.0");
+//
+//        // Send request to an API
+//        try (DataOutputStream dos = new DataOutputStream(conn.getOutputStream())) {
+//            dos.writeBytes(requestBody);
+//        }
+//
+//        System.out.println("Response code: " + conn.getResponseCode());
+//
+//        // Read Response from and API
+//        try (BufferedReader bf = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
+//            String line;
+//            while ((line = bf.readLine()) != null) {
+//                System.out.println(line);
+//            }
+//        }
 
     }
 
