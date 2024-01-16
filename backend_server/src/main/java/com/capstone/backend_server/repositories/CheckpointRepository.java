46da6d99b4ed8fe1f2a645a8e7aa9086554ecc65
Rosie Miall <rosiemiall38@gmail.com>
2024-01-16 18:10:03 +0000

    added checkpoint repository and service to create a patch request for marking a checkpoint as completed using derived query.

--- backend_server/src/main/java/com/capstone/backend_server/repositories/CheckpointRepository.java
+++ backend_server/src/main/java/com/capstone/backend_server/repositories/CheckpointRepository.java
@@ -4,6 +4,9 @@ import com.capstone.backend_server.models.Checkpoint;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CheckpointRepository extends JpaRepository<Checkpoint, Long> {
+    List<Checkpoint> findAllByRouteIdAndCompleted(Long routeId, boolean completed);
 }
